0a9ac9b5d869c77f2d9aa330535b3ad08265bfd2
yskta <kitapo3110@gmail.com>
2025-02-13 21:25:48 +0900

    add:ホーム/ログイン/サインアップ画面遷移

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import axios from 'axios';
 import { LoginPage } from './pages/LoginPage';
 import { SignupPage } from './pages/SignupPage';
@@ -19,6 +19,25 @@ function App() {
 
   return (
     <BrowserRouter>
+      <nav className="p-4 bg-white shadow">
+        <div className="flex gap-4">
+          <Link to="/" className="font-bold text-lg">ホーム</Link>
+          <div className="flex-1"></div>
+          <Link 
+            to="/login" 
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            ログイン
+          </Link>
+          <div className="flex-1"></div>
+          <Link 
+            to="/signup" 
+            className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
+          >
+            サインアップ
+          </Link>
+        </div>
+      </nav>
       <div className="min-h-screen bg-gray-100">
         {/* ヘルスチェックの表示 */}
         <div className="p-4">
